a1889ca8833c0205fdb9551a2ce4a46fe5f7ddc2
Corin Langosch <corin.langosch@netskin.com>
2011-07-05 14:00:29 +0200

    added android (eclipse) plattform

---
+++ android/src/com/example/helloworld/HelloWorldActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class HelloWorldActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
